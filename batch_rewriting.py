0c3d927a25d5f729437b02efdd185c24289887a4
aeaziz <azizelmejor@gmail.com>
2020-05-29 16:52:21 +0200

    Improved batch rewriting

--- batch_rewriting.py
+++ batch_rewriting.py
@@ -2,11 +2,19 @@ from cqapk_to_datalog.rewriting import rewrite
 from cqapk_to_datalog.parsers.cq_parser import parse_queries_from_file
 import sys
 
+def format_output(q):
+    free = ""
+    if len(q.free_vars) > 0:
+        free = "("+",".join(map(lambda x : x.name, q.free_vars))+")"
+    output = "\n% Datalog rewriting of CERTAINTY(q"+free+") with q = "+str(q)+"\n"
+    output += str(rewrite(q))
+    return output
+
+
 class PrintOutput:
     def save(self, queries):
         for q in queries:
-            print("\nDatalog rewriting of CERTAINTY(q) with q = "+str(q)+"\n")
-            print(rewrite(q))
+            print(format_output(q))
 
 class FileOutput:
     def __init__(self, file):
@@ -14,8 +22,7 @@ class FileOutput:
     def save(self, queries):
         file = open(self.file, 'w+')
         for q in queries:
-            file.write("\nDatalog rewriting of CERTAINTY(q) with q = "+str(q)+"\n")
-            file.write(str(rewrite(q)))
+            file.write(format_output(q))
 
 if len(sys.argv) < 2:
     print("Please give a valid file as input")
